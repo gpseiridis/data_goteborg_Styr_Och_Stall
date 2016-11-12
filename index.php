6d3c3630c487e79d9a7070f89bc45029b5a1b051
GomUser1 <g.pseiridis@gmail.com>
2016-11-12 13:54:24 +0100

    added dropdown menu, fixed the timestamp

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
-<?php
+</!DOCTYPE html>
+<html>
+<head>
+	<title>Styr Och Stall</title>
+</head>
+<body>
+ <div align="center">
+ <h1> Select a station</h1>
 
 
-$jsondata = file_get_contents('http://data.goteborg.se/StyrOchStall/v0.1/GetBikeStations/bbfb13cc-994c-4a5a-924e-85a5760b6e4c?format=json');
-$json = json_decode($jsondata, true);
-$output = "<ul>";
-$php_timestamp =  $json['TimeStamp'];
 
-//getting timestamp in readable form
-$php_timestamp_stripped= strstr($php_timestamp, '+', true);
-$php_timestamp_stripped= substr($php_timestamp_stripped,6);
-$php_timestamp_date = date("d F Y", $php_timestamp_stripped);
-
-echo "The timestamp in a readable format is ".$php_timestamp_date."";
-echo "<h1> Created on ".$php_timestamp_date."</h1>";
-
-foreach ($json['Stations'] as $station ) {
 	
-	$output .= "<h4> STATION: " .$station['Label']."</h4>";
-	$output .= "<li> Capacity: " .$station['Capacity']."</li>";
-	$output .= "<li> Available Bikes: " .$station['FreeBikes']."</li>";
-	$output .= "<li> Available stands: " .$station['FreeStands']."</li>";
-	$output .= "<li> Current status: " .$station['State']."</li>";
 
+ <form name="myform" action="display.php" method="POST">
+	
+		<select name="availableStations">
+			<option value="all">All</option>
+			<?php 
+				$array = explode( "\n", file_get_contents( "availableStations.php" ) );
+				$array_length = sizeof($array);
+			for ($i=0; $i < $array_length; $i++) { ?>	
+					
 
-}
+			<option value="<?php echo $i; ?>" >  <?php echo $array[$i]; ?>   </option>
+			<?php } ?>
+		</select>
+			
 
-	$output.="</ul>";
-	echo $output;
+		<input type="submit" value="Submit">
+	</form>
+</div>
 
-
+</body>
+</html>
